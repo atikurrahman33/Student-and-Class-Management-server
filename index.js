5052d6324fe1a129998f890da55f1a906f6ee764
atikurrahman33 <foysalatik33@gmail.com>
2023-12-04 13:58:39 +0600

    post class and get class

--- index.js
+++ index.js
@@ -32,6 +32,7 @@ async function run() {
     const classCollection = client.db("eduMentor").collection("allClass");
     const userCollection = client.db("eduMentor").collection("users");
     const reviewCollection = client.db("eduMentor").collection("review");
+    const requestCollection = client.db("eduMentor").collection("requests");
 
        // jwt related Api
 
@@ -106,6 +107,77 @@ app.post('/users',async(req,res)=>{
         const result =await classCollection.findOne(query ,options);
         res.send(result)
       })
+
+      // teacher Request Api
+
+      app.post('/requests',async(req,res)=>{
+        const newFood= req.body
+        console.log(newFood)
+        const result =await requestCollection.insertOne(newFood)
+        res.send(result)
+      })
+
+      // Add class
+
+      app.post('/addclass',async(req,res)=>{
+        const newFood= req.body
+        console.log(newFood)
+        const result =await classCollection.insertOne(newFood)
+        res.send(result)
+      })
+
+      // manage class
+
+      app.get('/addclass', async (req, res) => {
+        console.log(req.query);
+        let query = {};
+        if (req.query?.email) {
+          query = { email: req.query.email }
+        }
+        const cursor = classCollection.find(query);
+        const result = await cursor.toArray();
+        res.send(result);
+      });
+  
+  
+  
+      app.delete('/addclass/:id', async(req, res) => {
+        const id= req.params.id
+        console.log('delete id',id)
+        const query = {_id:new ObjectId(id)  };
+        const result = await classCollection.deleteOne(query);
+        res.send(result);    
+      })
+
+      app.patch('/addclass/:id', async (req, res) => {
+        const id = req.params.id;
+        const filter = { _id: new ObjectId(id) };
+        const options = { upsert: true };
+        const updateFood = req.body;
+        console.log(updateFood);
+        const foods = {
+          $set: {
+            name: updateFood.name,
+            food_image: updateFood.food_image,
+            status: updateFood.status,
+            quantity: updateFood.quantity,
+            location: updateFood.location,
+  
+          }
+        }
+  
+        const result = await foodCollection.updateOne(filter, foods, options);
+        res.send(result)
+      })
+  
+    
+    
+    
+
+
+     
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
