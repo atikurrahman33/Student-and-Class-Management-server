ac3802ccd75a108bb487987b90aa4ad1b834424e
atikurrahman33 <foysalatik33@gmail.com>
2023-12-04 23:10:42 +0600

    Final work

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const app = express() 
-require('dotenv').config() 
-var jwt = require('jsonwebtoken'); 
+const app = express()
+require('dotenv').config()
+var jwt = require('jsonwebtoken');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 
@@ -34,148 +34,189 @@ async function run() {
     const reviewCollection = client.db("eduMentor").collection("review");
     const requestCollection = client.db("eduMentor").collection("requests");
 
-       // jwt related Api
+    // jwt related Api
 
-       app.post('/jwt',async(req,res)=>{
-        const user =req.body;
-        console.log(req.headers)
-        const token =jwt.sign(user,process.env.ACCESS_TOKEN ,{expiresIn:'1h'})
-        res.send({ token })
-      })
+    app.post('/jwt', async (req, res) => {
+      const user = req.body;
+      console.log(req.headers)
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN, { expiresIn: '1h' })
+      res.send({ token })
+    })
 
 
-       // middleWare
+    // middleWare
 
-       const verifyToken= (req,res,next)=>{
-        console.log('khauyweghfyre',req.headers.authorization)
-        if(!req.headers.authorization){
-          return res.status(401).send({message: 'forbidden access'})
-  
-        }
-        const token =req.headers.authorization.split(' ')[1]
-        jwt.verify(token , process.env.ACCESS_TOKEN,(err, decoded)=>{
-          if(err){
-            return res.status(401).send({message: 'forbidden access'})
-          }
-          req.decoded= decoded;
-          next()
-        })
+    const verifyToken = (req, res, next) => {
+      console.log('khauyweghfyre', req.headers.authorization)
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: 'forbidden access' })
+
       }
-  
-    
-//   user APi
-
-app.post('/users',async(req,res)=>{
-    const user =req.body;
-    // insert email if use dose not exits
-    const query={email : user.email}
-    const existingUser= await userCollection.findOne(query);
-    if(existingUser){
-      return res.send({message:'user Already exists', insertedId:null})
+      const token = req.headers.authorization.split(' ')[1]
+      jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: 'forbidden access' })
+        }
+        req.decoded = decoded;
+        next()
+      })
     }
-    const result =await userCollection.insertOne(user);
-    res.send(result)
-  })
-
-  app.get('/users',async(req,res)=>{
-    const email =req.query.email;
-    const quary ={email:email}
-    const result= await userCollection.find(quary).toArray();
-    res.send(result)   
-})
 
 
-    app.get('/allClass' ,async(req,res)=>{
-      const cursor =classCollection.find();
+    //   user APi
+
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      // insert email if use dose not exits
+      const query = { email: user.email }
+      const existingUser = await userCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: 'user Already exists', insertedId: null })
+      }
+      const result = await userCollection.insertOne(user);
+      res.send(result)
+    })
+
+    app.get('/users', async (req, res) => {
+      const result = await userCollection.find().sort({ _id: -1 }).toArray();
+      res.send(result);
+    })
+
+    app.get('/users', async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email }
+      const result = await userCollection.find(query).toArray();
+      res.send(result)
+    })
+
+
+    app.get('/allClass', async (req, res) => {
+      const cursor = classCollection.find();
       const result = await cursor.toArray();
       res.send(result)
     })
 
-    app.get('/allreview' ,async(req,res)=>{
-        const cursor =reviewCollection.find();
-        const result = await cursor.toArray();
-        res.send(result)
-      })
+    app.get('/allreview', async (req, res) => {
+      const cursor = reviewCollection.find();
+      const result = await cursor.toArray();
+      res.send(result)
+    })
 
-    app.get('/allClass/:id', async(req,res)=>{
-        const id = req.params.id;
-        const query = { _id: new ObjectId(id)}
-        const options = {
-       
-          projection: {  title:1,instructor:1, category: 1,rating: 1,price: 1,description: 1,image: 1,total_enrollment: 1 },
-        };
-        const result =await classCollection.findOne(query ,options);
-        res.send(result)
-      })
+    app.get('/allClass/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const options = {
 
-      // teacher Request Api
+        projection: { title: 1, instructor: 1, category: 1, rating: 1, price: 1, description: 1, image: 1, total_enrollment: 1 },
+      };
+      const result = await classCollection.findOne(query, options);
+      res.send(result)
+    })
 
-      app.post('/requests',async(req,res)=>{
-        const newFood= req.body
-        console.log(newFood)
-        const result =await requestCollection.insertOne(newFood)
-        res.send(result)
-      })
+    // teacher Request Api
 
-      // Add class
+    app.post('/requests', async (req, res) => {
+      const newFood = req.body
+      console.log(newFood)
+      const result = await requestCollection.insertOne(newFood)
+      res.send(result)
+    })
 
-      app.post('/addclass',async(req,res)=>{
-        const newFood= req.body
-        console.log(newFood)
-        const result =await classCollection.insertOne(newFood)
-        res.send(result)
-      })
+    // Add class
 
-      // manage class
+    app.post('/addclass', async (req, res) => {
+      const newFood = req.body
+      console.log(newFood)
+      const result = await classCollection.insertOne(newFood)
+      res.send(result)
+    })
 
-      app.get('/addclass', async (req, res) => {
-        console.log(req.query);
-        let query = {};
-        if (req.query?.email) {
-          query = { email: req.query.email }
+    // manage class
+
+    app.get('/addclass', async (req, res) => {
+      console.log(req.query);
+      let query = {};
+      if (req.query?.email) {
+        query = { email: req.query.email }
+      }
+      const cursor = classCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    //http://localhost:5000/allClass/approve/:id
+    app.patch('/allClass/approve/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          status: 'approved'
+        },
+      };
+      const result = await classCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    })
+    app.delete('/addclass/:id', async (req, res) => {
+      const id = req.params.id
+      console.log('delete id', id)
+      const query = { _id: new ObjectId(id) };
+      const result = await classCollection.deleteOne(query);
+      res.send(result);
+    })
+
+    app.patch('/updateClass/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updatedClass = req.body;
+      
+      const sClass = {
+        $set: {
+          title: updatedClass.title,
+          price: updatedClass.price,
+          total_enrollment: updatedClass.total_enrollment,
+          description: updatedClass.description
+
         }
-        const cursor = classCollection.find(query);
-        const result = await cursor.toArray();
-        res.send(result);
-      });
-  
-  
-  
-      app.delete('/addclass/:id', async(req, res) => {
-        const id= req.params.id
-        console.log('delete id',id)
-        const query = {_id:new ObjectId(id)  };
-        const result = await classCollection.deleteOne(query);
-        res.send(result);    
-      })
+      }
 
-      app.patch('/addclass/:id', async (req, res) => {
-        const id = req.params.id;
-        const filter = { _id: new ObjectId(id) };
-        const options = { upsert: true };
-        const updateFood = req.body;
-        console.log(updateFood);
-        const foods = {
-          $set: {
-            name: updateFood.name,
-            food_image: updateFood.food_image,
-            status: updateFood.status,
-            quantity: updateFood.quantity,
-            location: updateFood.location,
-  
-          }
-        }
-  
-        const result = await foodCollection.updateOne(filter, foods, options);
-        res.send(result)
-      })
-  
-    
-    
-    
+      const result = await classCollection.updateOne(filter, sClass, options);
+      res.send(result)
+    })
+
+    // http://localhost:5000/users/admin/ashraful.islam0871@gmail.com
+    app.get('/users/admin/:email', async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email }
+      const user = await userCollection.findOne(query);
+      // console.log(user);
+      const result = { admin: user?.role === 'admin' }
+      res.send(result)
+    })
+
+    app.patch('/users/admin/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: 'admin'
+        },
+      };
+      const result = await userCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    })
+
+    app.get('/users/instructor/:email', async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email }
+      const user = await userCollection.findOne(query);
+      // console.log(user);
+      const result = { instructor: user?.role === 'instructor' }
+      res.send(result)
+    })
 
 
-     
+
+
 
 
     // Send a ping to confirm a successful connection
